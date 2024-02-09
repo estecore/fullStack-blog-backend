5e282901fa89a512dc262da7e6b69a486134a181
estecore <estecore@yandex.ru>
2024-02-10 01:07:08 +0400

    create user-model

--- index.js
+++ index.js
@@ -13,27 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      password: req.body.password,
-      fullName: "Vasya Pupkin",
-    },
-    "secret123"
-  );
-
-  res.json({
-    success: true,
-    token,
-  });
-});
+app.post("/auth/register", (req, res) => {});
 
 app.listen(4444, (err) => {
   if (err) {
