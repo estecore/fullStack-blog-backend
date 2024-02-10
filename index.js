e25670068df6413594dd8c12a5f96b2dded321e5
estecore <estecore@yandex.ru>
2024-02-10 17:40:51 +0400

    add cors for frontend get posts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import {
   registerValidation,
@@ -32,6 +33,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
 app.post(
